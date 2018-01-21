a364e340e2b66072fbc0849417d87e576a2daa54
Christoph Deil <Deil.Christoph@gmail.com>
2018-01-21 18:37:51 +0100

    Add html/latest

---
+++ html/latest/_static/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "gammapy",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://docs.gammapy.org/en/latest/",
+    theme: "bootstrap-astropy",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "ef1c4e42"
+  };
+  
